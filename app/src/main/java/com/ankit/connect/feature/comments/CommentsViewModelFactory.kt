ab0244dafcc628cafd24c4b97187984810343ca3
Ankit <apsaliya@gmail.com>
2018-04-19 05:31:42 +0530

    remove unused resources.

--- app/src/main/java/com/ankit/connect/feature/comments/CommentsViewModelFactory.kt
+++ app/src/main/java/com/ankit/connect/feature/comments/CommentsViewModelFactory.kt
@@ -9,7 +9,7 @@ import com.ankit.connect.feature.comments.ui.CommentsViewModel
 internal class CommentsViewModelFactory(val remoteDataSource: RemoteDataSource, val app: App) : ViewModelProvider.Factory {
   override fun <T : ViewModel?> create(modelClass: Class<T>): T {
     if (modelClass.isAssignableFrom(CommentsViewModel::class.java)) {
-      return modelClass.getConstructor(RemoteDataSource::class.java).newInstance(remoteDataSource, app)
+      return modelClass.getConstructor(RemoteDataSource::class.java, App::class.java).newInstance(remoteDataSource, app)
     }
     throw IllegalStateException("Unknown ViewModel class.")
   }
--- app/src/main/java/com/ankit/connect/feature/feed/FeedViewModelFactory.kt
+++ app/src/main/java/com/ankit/connect/feature/feed/FeedViewModelFactory.kt
@@ -9,7 +9,7 @@ import com.ankit.connect.feature.feed.ui.FeedViewModel
 internal class FeedViewModelFactory(private val remoteDataSource: RemoteDataSource, val app: App) : ViewModelProvider.Factory {
   override fun <T : ViewModel?> create(modelClass: Class<T>): T {
     if (modelClass.isAssignableFrom(FeedViewModel::class.java)) {
-      return modelClass.getConstructor(RemoteDataSource::class.java).newInstance(remoteDataSource, app)
+      return modelClass.getConstructor(RemoteDataSource::class.java, App::class.java).newInstance(remoteDataSource, app)
     }
     throw IllegalStateException("Unknown ViewModel class.")
   }
